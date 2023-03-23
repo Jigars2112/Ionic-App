3ae8bf808eee0b043101ccd5ec4ad3280f333e5f
Jigars2112 <111821142+Jigars2112@users.noreply.github.com>
2023-03-23 11:11:48 -0400

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'JS Ionic21',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
